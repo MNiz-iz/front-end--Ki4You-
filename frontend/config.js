b1ce275d93b9b764472dd4162fa258ec321bd03d
Arisa PLoyprapt <84123791+MNiz-iz@users.noreply.github.com>
2023-06-28 20:23:43 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
+//change url for call backend api or call frontend domain
 var url = {
-    "url_backend" : "http://localhost:8888",
+    "url_backend" : "http://localhost:8888", 
     "url_frontend" : "http://127.0.0.1:5500"
 };
